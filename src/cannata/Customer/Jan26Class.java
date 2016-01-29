9c0e3b79d0d929e5d4e49372640edd1613813b2f
Wilson Lu <wklu169@gmail.com>
2016-01-28 18:28:57 -0600

    Changing files

---
+++ src/cannata/Customer/Jan26Class.java
@@ -0,0 +1,10 @@
+package cannata;
+
+public class Jan26Class {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
